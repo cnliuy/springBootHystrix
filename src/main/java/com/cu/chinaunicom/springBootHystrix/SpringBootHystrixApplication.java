ded8672b31172b751a8ebaef810912fbb90e4af2
ly <ly@DESKTOPLY>
2018-03-01 14:19:15 +0800

    熔断器

--- src/main/java/com/cu/chinaunicom/springBootHystrix/SpringBootHystrixApplication.java
+++ src/main/java/com/cu/chinaunicom/springBootHystrix/SpringBootHystrixApplication.java
@@ -2,8 +2,12 @@ package com.cu.chinaunicom.springBootHystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@SpringBootApplication
+@SpringBootApplication 
+@EnableHystrixDashboard  //code change 0
+@EnableCircuitBreaker	//code change 1
 public class SpringBootHystrixApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/cu/chinaunicom/springBootHystrix/rest/HelloController.java
+++ src/main/java/com/cu/chinaunicom/springBootHystrix/rest/HelloController.java
@@ -1,20 +1,29 @@
 package com.cu.chinaunicom.springBootHystrix.rest;
 
 import java.util.Date;
-
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class HelloController {
+	
 	/**
-	 * http://127.0.0.1:6080/hello
+	 * 访问链接:
+	 * 		http://127.0.0.1:6080/hello
+	 * 
+	 * 
 	 * */
-    @RequestMapping(value = "hello", method = RequestMethod.GET)    
+	//code change 2
+	@HystrixCommand(fallbackMethod = "error", commandProperties = {
+	            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "10001")
+	})
+    @RequestMapping(value = "/hello", method = RequestMethod.GET, produces = {"application/json;charset=UTF-8"}) 
     public Object hello(HttpServletRequest request) {
+    	
     	int millis  = 10000;
     	Date date = new Date(); 
     	try {
@@ -26,5 +35,20 @@ public class HelloController {
 		} 
     	return  "helloWorld "+date;    	
     }
+	
+	
+	
+	
+	/**
+	 * 注意此函数中的入参 须与 hello()的一致
+	 *    才能被调用
+	 * 
+	 * */
+	//code change 3
+	public Object error(HttpServletRequest request) {
+		Date daterr = new Date(); 
+		return "hello error "+daterr;
+	}
+    
 
 }
